fb247cecd0e5e674872230eb309a284aa107a450
MJ-BUC <m.bucaro@outlook.com>
2022-03-29 15:29:04 -0500

    working js file to append text to a txt file

--- js/fileWriting.js
+++ js/fileWriting.js
@@ -1,17 +1,33 @@
-function WriteToFile(passForm) { 
+function Append(content) {
+    
+    const fs = require('fs');
+
+    fs.appendFile('C:\\Users\\lunke\\OneDrive\\Documents\\Github\\WebBrowser\\webdata.txt', content + "\n", err => {
+        if (err) {
+            console.err;
+            return;
+        }
+    });
+}
+
+// debugging for js file
+// Append("Cheese" + "\n");
+
+
+// function WriteToFile(passForm) { 
  
-    var fso = CreateObject("Scripting.FileSystemObject");  
-    var s = fso.CreateTextFile("C:\\Users\\lunke\\OneDrive\\Documents\\Github\\WebBrowser\\webdata.txt", True); 
+//     var fso = CreateObject("Scripting.FileSystemObject");  
+//     var s = fso.CreateTextFile("C:\\Users\\lunke\\OneDrive\\Documents\\Github\\WebBrowser\\webdata.txt", True); 
  
-    var firstName = document.getElementById('FirstName'); 
-    var lastName = document.getElementById('lastName'); 
+//     var firstName = document.getElementById('FirstName'); 
+//     var lastName = document.getElementById('lastName'); 
  
-    s.writeline("First Name :" + firstName); 
-    s.writeline("Last Name :" + lastName); 
+//     s.writeline("First Name :" + firstName); 
+//     s.writeline("Last Name :" + lastName); 
  
-    s.writeline("-----------------------------"); 
-    s.Close(); 
- }
+//     s.writeline("-----------------------------"); 
+//     s.Close(); 
+//  }
 
 
 // testing for js
