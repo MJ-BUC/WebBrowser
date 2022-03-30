25a9bccf2b43d55cea0241b4fb7e45dd0d0674fa
MJ-BUC <m.bucaro@outlook.com>
2022-03-30 18:47:44 -0500

    testing added for testing the flask library

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-import imp
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def webbrowser():
     return render_template('webbrowser.html')
+
+# @app.route('/', methods=['POST'])
+# def my_form_post():
+#     text = request.form['text']
+#     processed_text = text.upper()
+#     return processed_text
+
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
